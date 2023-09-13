92a6037292ebcae46b11cb0bf2ecaeb76729ae32
lirylinx <dercioguirruta44@gmail.com>
2023-09-13 21:04:58 +0200

    adicionado prefixo aos endpoints

--- lib/src/utils/app_endpoint.dart
+++ lib/src/utils/app_endpoint.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: constant_identifier_names
 
 class AppEndpoint {
-  static const TOKEN = '/TOKEN';
-  static const LISTA_ARTIGO = '/ArtigoController/Artigo/lista';
-  static const LISTA_CLIENTE = '/ClienteController/Cliente/lista';
-  static const ENCOMENDA = '';
-  static const VENDA = '';
+  static const TOKEN = '/webapi/token';
+  static const LISTA_ARTIGO = '/webapi/ArtigoController/Artigo/lista';
+  static const LISTA_CLIENTE = '/webapi/ClienteController/Cliente/lista';
+  static const ENCOMENDA = '/webapi/';
+  static const VENDA = '/webapi/';
 }
