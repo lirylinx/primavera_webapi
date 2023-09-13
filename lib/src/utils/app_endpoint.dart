68775d678ac3b20611ba6d3f4033252142e6f0ad
lirylinx <dercioguirruta44@gmail.com>
2023-09-13 16:39:05 +0200

    constantes com endpoints da webapi

---
+++ lib/src/utils/app_endpoint.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: constant_identifier_names
+
+class AppEndpoint {
+  static const TOKEN = '/TOKEN';
+  static const LISTA_ARTIGO = '/ArtigoController/Artigo/lista';
+  static const LISTA_CLIENTE = '/ClienteController/Cliente/lista';
+  static const ENCOMENDA = '';
+  static const VENDA = '';
+}
