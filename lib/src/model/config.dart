08c5c45342c77309fb6c76d4d768dce35d02948f
lirylinx <dercioguirruta44@gmail.com>
2023-09-18 09:09:53 +0200

    criar metodo para retorno de dado em Json e String

--- lib/src/model/config.dart
+++ lib/src/model/config.dart
@@ -13,14 +13,20 @@ class ConfigWebapi {
       required this.grantType,
       required this.line});
 
-  String toJson() {
-    return jsonEncode({
-      'company': company,
-      'instance': instance,
-      'grant_type': grantType,
-      'line': line,
-    });
-  }
+  @override
+  String toString() => jsonEncode({
+        'company': company,
+        'instance': instance,
+        'grant_type': grantType,
+        'line': line,
+      });
+
+  Map<String, dynamic> toJson() => {
+        'company': company,
+        'instance': instance,
+        'grant_type': grantType,
+        'line': line,
+      };
 }
 
 // modelo para configuração do POS
@@ -49,15 +55,35 @@ class ConfigPos {
   }
 }
 
-class ConfigSessao {
+class ConfigTokenSessao {
   final String accessToken;
   final String tokenType;
-  final String expireIn;
+  final int expireIn;
 
-  ConfigSessao(
+  ConfigTokenSessao(
       {required this.accessToken,
       required this.tokenType,
       required this.expireIn});
+
+  factory ConfigTokenSessao.fromJson(Map<String, dynamic> data) {
+    ConfigTokenSessao _token = ConfigTokenSessao(
+        accessToken: data['access_token'],
+        tokenType: data['token_type'],
+        expireIn: data['expires_in']);
+
+    return _token;
+  }
+
+  Map<String, dynamic> toJson() => {
+        'access_token': accessToken,
+        'token_type': tokenType,
+        'expires_in': expireIn
+      };
+
+  @override
+  String toString() {
+    return jsonEncode(toJson());
+  }
 }
 
 class ConfigServidor {
--- lib/src/model/utilizador.dart
+++ lib/src/model/utilizador.dart
@@ -1,6 +1,16 @@
+import 'dart:convert';
+
 class Utilizador {
   final String username;
   final String password;
 
   Utilizador({required this.username, required this.password});
+
+  Map<String, dynamic> toJson() => {
+        'username': username,
+        'password': password,
+      };
+
+  @override
+  String toString() => jsonEncode(toJson());
 }
