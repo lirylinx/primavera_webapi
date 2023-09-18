20fc64cfdb02eb4183dfd06afa1f43e17b456673
lirylinx <dercioguirruta44@gmail.com>
2023-09-18 09:11:45 +0200

    adicionar rota para lista de armazens

--- lib/src/utils/app_endpoint.dart
+++ lib/src/utils/app_endpoint.dart
@@ -2,7 +2,7 @@
 
 class AppEndpoint {
   static const TOKEN = '/webapi/token';
-  static const LISTA_ARTIGO = '/webapi/ArtigoController/Artigo/lista';
+  static const LISTA_ARTIGO = '/WebApi/ArtigoController/Artigo/listaarmazem';
   static const LISTA_CLIENTE = '/webapi/ClienteController/Cliente/lista';
   static const ENCOMENDA = '/webapi/';
   static const VENDA = '/webapi/';
