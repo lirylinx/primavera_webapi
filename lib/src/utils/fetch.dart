c50794e5b7f6f88302fe250e5c7cd21deb2ebd1f
lirylinx <dercioguirruta44@gmail.com>
2023-09-18 09:13:10 +0200

    Classe responsavel das requisições

---
+++ lib/src/utils/fetch.dart
@@ -0,0 +1,99 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:primavera_webapi/src/model/config.dart';
+import 'package:primavera_webapi/src/model/utilizador.dart';
+import 'package:primavera_webapi/src/utils/app_endpoint.dart';
+
+import '../model/artigo/artigo.dart';
+import '../model/cliente.dart';
+
+class Fetch {
+  final Utilizador utilizador =
+      Utilizador(username: 'jmr', password: 'jmr2013!');
+  final ConfigServidor servidor = ConfigServidor('165.90.83.186', 2018);
+  final ConfigWebapi webapi = ConfigWebapi(
+      company: 'terramar',
+      instance: 'default',
+      grantType: 'password',
+      line: 'professional');
+
+  ConfigTokenSessao? tokenSessao;
+
+  Fetch();
+
+  Future<ConfigTokenSessao> getToken() async {
+    var url = Uri.http(servidor.toString(), AppEndpoint.TOKEN);
+    Map<String, dynamic> data = webapi.toJson();
+    data.addAll(utilizador.toJson());
+
+    var response = await http.post(url, body: data);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    Map<String, dynamic> body = jsonDecode(response.body);
+    return ConfigTokenSessao.fromJson(body);
+  }
+
+  Future<List<Artigo>> getListaArtigo(String token) async {
+    var url = Uri.http(servidor.toString(), AppEndpoint.LISTA_ARTIGO);
+
+    var response = await http.get(url, headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    });
+
+    List<Artigo> _artigos = [];
+
+    if (response.statusCode == 200) {
+      dynamic data = jsonDecode(response.body);
+      data = jsonDecode(data)["DataSet"]["Table"];
+
+      for (dynamic rawArtigo in data) {
+        Artigo artigo = Artigo.fromJson(rawArtigo);
+        _artigos.add(artigo);
+      }
+    } else if (response.statusCode == 401) {
+      // renovar o token apos expirar
+      ConfigTokenSessao tokenSessao = await getToken();
+      getListaArtigo(tokenSessao.accessToken);
+    } else {
+      _artigos = [];
+    }
+
+    return _artigos;
+  }
+
+  Future<List<Cliente>> getListaCliente(String token) async {
+    var url = Uri.http(servidor.toString(), AppEndpoint.LISTA_CLIENTE);
+
+    var response = await http.get(url, headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    });
+
+    List<Cliente> listaCliente = [];
+
+    if (response.statusCode == 200) {
+      dynamic data = jsonDecode(response.body);
+      data = jsonDecode(data)["DataSet"]["Table"];
+
+      for (dynamic rawCliente in data) {
+        Cliente _cliente = Cliente.fromJson(rawCliente);
+        listaCliente.add(_cliente);
+      }
+
+      print(listaCliente);
+    } else if (response.statusCode == 401) {
+      // renovar o token apos expirar
+      ConfigTokenSessao tokenSessao = await getToken();
+      getListaCliente(tokenSessao.accessToken);
+    } else {
+      listaCliente = [];
+    }
+
+    return listaCliente;
+  }
+}
