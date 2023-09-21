4b1c9335a8e3959a207ec51896b7ed4aa42f7a9d
lirylinx <dercioguirruta44@gmail.com>
2023-09-21 13:33:32 +0200

    teste dos metodos

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:primavera_webapi/src/model/artigo/artigo.dart';
 import 'package:primavera_webapi/src/utils/fetch.dart';
 
 Future<void> main(List<String> args) async {
   Fetch().getToken().then((data) async {
-    await Fetch().listaCliente(data.accessToken);
+    // await Fetch().criarEncomenda(
+    //     data.accessToken, 'ecw', 2023, 'ADA001', DateTime.now().toString(), [
+    //   Artigo(artigo: '002', quantidade: 2),
+    //   Artigo(artigo: '00251', quantidade: 5)
+    // ]);
+
+    // await Fetch().abrirPosCaixa(data.accessToken);
+    // await Fetch().fecharPosCaixa(data.accessToken, 'cm1');
   });
 }
