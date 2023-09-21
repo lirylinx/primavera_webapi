a6bd4f2c42be3fe4372c6a58f29757652b267fe7
lirylinx <dercioguirruta44@gmail.com>
2023-09-21 14:56:51 +0200

    export modelo e utilitarios

--- example/primavera_webapi_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:primavera_webapi/primavera_webapi.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- lib/primavera_webapi.dart
+++ lib/primavera_webapi.dart
@@ -3,6 +3,18 @@
 /// More dartdocs go here.
 library;
 
-export 'src/primavera_webapi_base.dart';
+// Modelos
+export 'src/model/artigo/artigo.dart';
+export 'src/model/artigo/artigo_armazem.dart';
+export 'src/model/artigo/artigo_lote.dart';
+export 'src/model/pos/venda.dart';
+export 'src/model/encomenda.dart';
+export 'src/model/cliente.dart';
+export 'src/model/utilizador.dart';
+export 'src/model/endereco.dart';
+export 'src/model/regras_negocio.dart';
+export 'src/model/config.dart';
 
-// TODO: Export any libraries intended for clients of this package.
+// Metodos uteis
+export 'src/utils/app_endpoint.dart';
+export 'src/utils/fetch.dart';
